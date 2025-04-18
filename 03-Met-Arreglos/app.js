0dbed7209af7cc0833659c41f458a227be2b88ff
araceli <aracelicruzvazquez19@gmail.com>
2025-04-17 23:44:24 -0600

    modificacion de datos y funciones

--- 03-Met-Arreglos/app.js
+++ 03-Met-Arreglos/app.js
@@ -1,14 +1,14 @@
 const productos = [
-    { nombre: "Camiseta", precio: 15, categoria: "Ropa" },
-    { nombre: "Laptop", precio: 800, categoria: "Electrónica" },
-    { nombre: "Libro", precio: 12, categoria: "Educación" },
-    { nombre: "Zapatos", precio: 50, categoria: "Ropa" },
-    { nombre: "Celular", precio: 600, categoria: "Electrónica" },
+    { nombre: "pechiga de pollo", precio: 60, categoria: "alimentos" },
+    { nombre: "llantas", precio: 800, categoria: "automotriz" },
+    { nombre: "Zapatillas negras", precio: 500, categoria: "calzado" },
+    { nombre: "Leche de Almendras", precio: 30, categoria: "lacteos" },
+    { nombre: "chayote", precio: 10, categoria: "verdura" },
 ];
 
 // Filtrar productos que cuesten menos de $100
-const productosBaratos = productos.filter(producto => producto.precio < 100);
-console.log("Productos que cuestan menos de $100:");
+const productosBaratos = productos.filter(producto => producto.precio < 1000);
+console.log("Productos que cuestan menos de $1000:");
 productosBaratos.forEach(producto => {
     console.log(`- ${producto.nombre}, $${producto.precio}, Categoría: ${producto.categoria}`);
 });
@@ -29,11 +29,11 @@ nombresProductos.forEach(nombre => console.log(`- ${nombre}`));
 const precioTotal = productosBaratos.reduce((total, producto) => total + producto.precio, 0);
 console.log(`\nPrecio total de los productos baratos: $${precioTotal}`);
 
-const hayRopaBarata = productosBaratos.some(producto => producto.categoria === "Ropa");
-console.log(`¿Hay algún producto barato en la categoría 'Ropa'? ${hayRopaBarata ? "Sí" : "No"}`);
+const haycalzadoBarata = productosBaratos.some(producto => producto.categoria === "calzado");
+console.log(`¿Hay algún producto barato en la categoría 'calzado'? ${haycalzadoBarata ? "Sí" : "No"}`);
 
 const todosMuyBaratos = productosBaratos.every(producto => producto.precio < 50);
 console.log(`¿Todos los productos baratos cuestan menos de $50? ${todosMuyBaratos ? "Sí" : "No"}`);
 
-const incluyeLibro = nombresProductos.includes("Libro");
-console.log(`¿La lista incluye el producto 'Libro'? ${incluyeLibro ? "Sí" : "No"}`);
+const incluyellantas = nombresProductos.includes("llantas");
+console.log(`¿La lista incluye el producto 'llantas'? ${incluyellantas ? "Sí" : "No"}`);
